7d37b372b86fe3fe157c24091622dd96b5211592
Crown <crown-huang@foxmail.com>
2017-09-18 12:49:01 +0800

    add num2place

--- MissileFire/InitialGraph.py
+++ MissileFire/InitialGraph.py
@@ -275,3 +275,19 @@ def dijkstra(graph, src):
         nodes.remove(k)
         # print(visited, nodes)  # 输出节点的添加过程
     return distance, path
+
+
+def num2place(places):
+    for p in places:
+        if p == 0:
+            return "D1"
+        elif p == 1:
+            return "D2"
+        elif 2 <= p < 8:
+            return "Z0%d" % (p - 1)
+        elif 8 <= p < 68:
+            return "F%d" % (p - 7)
+        elif 68 <= p < 130:
+            return "J%d" % (p - 67)
+
+
